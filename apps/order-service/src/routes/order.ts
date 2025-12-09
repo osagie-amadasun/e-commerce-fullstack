f4b2b845e7ed177b268d14811de3f6a20a2a7139
Osagie Amadasun <osagieamadasun123@gmail.com>
2025-12-09 18:18:24 +0100

    added order route and installed product-db  export, then installed it in the product-service json, thereby linking the 2 together as i had forgotten to do so and thought it was going to fix the prisma issue but apparently did not

--- apps/order-service/src/routes/order.ts
+++ apps/order-service/src/routes/order.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { shouldBeUser } from "../middleware/authMiddleware";
+import { Order } from "@repo/order-db"
 
 export const orderRoute = async (fastify: FastifyInstance) => {
-  fastify.get("/user-order", async (request:FastifyRequest, reply:FastifyReply) => {
-
+  fastify.get("/user-order", {preHandler: shouldBeUser}, async (request:FastifyRequest, reply:FastifyReply) => {
+    const order = await Order
   });
 };
